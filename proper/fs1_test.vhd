9cd15002b4be6de1fa00b19fe9556d66c50f1667
Ragnar Rebase <rrebase@gmail.com>
2019-01-06 23:28:57 +0200

    Update fs1.

--- proper/fs1_test.vhd
+++ proper/fs1_test.vhd
@@ -1,8 +1,3 @@
-------------------------------------------------------------------------
--- IAY0150 - Homework #1. Test bench for the example task.
-------------------------------------------------------------------------
--- (C) Peeter Ellervee - 2016 - Tallinn
-------------------------------------------------------------------------
 library IEEE; use IEEE.std_logic_1164.all;
 entity test is
 end entity test;
@@ -19,7 +14,6 @@ architecture bench of test is
   end component;
   for U1: f_system use entity work.f_system(tabel);
   for U2: f_system use entity work.f_system(espresso);
-  for U3: f_system use entity work.f_system(opti);
 
   function compare_signals (s1, s2, s3: std_logic) return std_logic is
   begin
